4528854362b79c96aea64e6b7d5450d23265b52c
Mateusz Gierczak <mateusz.gierczak33@gmail.com>
2018-05-18 20:56:01 +0200

    Implement client connection with server

--- Client/Source/main.cpp
+++ Client/Source/main.cpp
@@ -8,6 +8,7 @@ int main(int argv, char* argc[])
     QCoreApplication app {argv, argc};
 
     ServerSettings settings {"127.0.0.1", 12345};
+    Client client {settings};
 
     return app.exec();
 }
